b689fb861d9f095abcef1e5b8aafea64395719ee
John Kua <john@kua.fm>
2014-05-10 00:20:30 -0700

    Update geographiclib.rb to use latest version (1.35)

--- geographiclib.rb
+++ geographiclib.rb
@@ -1,9 +1,10 @@
 require 'formula'
 
 class Geographiclib < Formula
-  url 'http://downloads.sourceforge.net/project/geographiclib/distrib/GeographicLib-1.27.tar.gz'
+  #url 'http://downloads.sourceforge.net/project/geographiclib/distrib/GeographicLib-1.27.tar.gz'
+  url 'http://downloads.sourceforge.net/project/geographiclib/distrib/GeographicLib-1.35.tar.gz'
   homepage 'http://geographiclib.sourceforge.net/'
-  sha1 '6dfdaead49267b8d96c450686f68345e5ab190f3'
+  sha1 '1108a88a124c30e87bf86683de7d74eea7dafc11'
 
   def install
     system "./configure", "--disable-dependency-tracking",

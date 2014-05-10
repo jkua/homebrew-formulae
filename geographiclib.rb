618a6163d0851d5514b20df875df769eb4fc488b
John Kua <john@kua.fm>
2014-05-10 00:59:02 -0700

    Update geographiclib.rb

--- geographiclib.rb
+++ geographiclib.rb
@@ -7,6 +7,11 @@ class Geographiclib < Formula
   sha1 '1108a88a124c30e87bf86683de7d74eea7dafc11'
 
   def install
+    ENV.append "CXXFLAGS", '-stdlib=libstdc++'
+    ENV.append "CFLAGS", '-stdlib=libstdc++'
+    ENV.append "LDFLAGS", '-stdlib=libstdc++ -lstdc++'
+    #The following is necessary because libtool liks to strip LDFLAGS:
+    ENV.cxx = "/usr/bin/clang -stdlib=libstdc++
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
